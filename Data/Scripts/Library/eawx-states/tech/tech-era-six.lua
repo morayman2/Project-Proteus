ce2c06fcb094032e9a74a405d7cc0e62989c76d6
Destruction1214 <249353461+Destruction1214@users.noreply.github.com>
2026-03-19 17:59:43 +0100

    more groups for IPV swap
    -added Lambda and Arda

--- Data/Scripts/Library/eawx-states/tech/tech-era-six.lua
+++ Data/Scripts/Library/eawx-states/tech/tech-era-six.lua
@@ -172,7 +172,8 @@ return {
         local proteus = GlobalValue.Get("PROTEUS_GROUP_NAME")
         --if proteus == ("RAYTER" or "ELROOD" or "ISECTOR" or "TAGGE" or "BAKURA" or "TAPANI" or "PRAKITH" or "CATO_NEIMOIDIA") then
         if proteus == "RAYTER" or proteus == "ELROOD" or proteus == "ISECTOR" or proteus == "TAGGE"
-            or proteus == "BAKURA" or proteus == "TAPANI" or proteus == "PRAKITH" or proteus == "CATO_NEIMOIDIA" then
+            or proteus == "BAKURA" or proteus == "TAPANI" or proteus == "PRAKITH" or proteus == "CATO_NEIMOIDIA" 
+            or proteus == "ARDA" or proteus == "LAMBDA" then
             UnitUtil.SetLockList("IMPERIAL_PROTEUS", {
                 "IPV4"
             })
