a9b7dea240940b964e0aa43f81df669e5e5dfc10
Greene7z <55987497+Greene7z@users.noreply.github.com>
2026-03-23 15:26:54 +0000

    Fix for shipmarket + Acc II Supercruiser
    * Add proteus group check to shipmarket display faction, minor function rename.
    * Fix syntax for Acc II supercruiser gameobject

--- Data/Scripts/Library/eawx-mod-icw/gameobjects/ACCLAMATOR_II_SUPERCRUISER.lua
+++ Data/Scripts/Library/eawx-mod-icw/gameobjects/ACCLAMATOR_II_SUPERCRUISER.lua
@@ -2,7 +2,7 @@ return {
 	Ship_Crew_Requirement = 155,
 	Fighters = {
 		["ELITE_FIGHTERBOMBER"] = {
-			DEFAULT = {Initial = 1, Reserve = 2},}
+			DEFAULT = {Initial = 1, Reserve = 2},
 		},
 	},
 	Native = "IMPERIAL",
