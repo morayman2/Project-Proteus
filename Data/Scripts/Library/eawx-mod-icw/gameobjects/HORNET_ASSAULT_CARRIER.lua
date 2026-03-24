109cbc489aad9a97232740aafdb02ba648270fff
Greene7z <55987497+Greene7z@users.noreply.github.com>
2026-03-24 23:44:16 +0000

    Tapani units + minor fixes
    * Added Tapani unit data + lua data, currently missing art assets
    * Fix to fighter hardpoint XML data being closed too early
    * Minor XML hardpoint XML format changes
    * Removed accidental workspace file

---
+++ Data/Scripts/Library/eawx-mod-icw/gameobjects/HORNET_ASSAULT_CARRIER.lua
@@ -0,0 +1,16 @@
+return {
+    Ship_Crew_Requirement = 132,
+    Fighters = {
+        ["LIGHT_FIGHTER"] = {
+            DEFAULT = {Initial = 1, Reserve = 1}
+        },
+        ["ELITE_FIGHTERBOMBER_DOUBLE"] = {
+            DEFAULT = {Initial = 1, Reserve = 1}
+        },
+        ["BOMBER2_DOUBLE"] = {
+            DEFAULT = {Initial = 1, Reserve = 1}
+        }
+    },
+	Native = "CORPORATE_SECTOR",
+    Scripts = {"multilayer", "single-unit-retreat", "fighter-spawn"}
+}
---
+++ Data/Scripts/Library/eawx-mod-icw/gameobjects/MANSK_FRIGATE.lua
@@ -0,0 +1,5 @@
+return {
+    Ship_Crew_Requirement = 18,
+    Fighters = {},
+    Scripts = {"multilayer", "single-unit-retreat"}
+}
