d0bc529b876e159721dfc5e0321a9afd58c53cec
Eleian <swjacklee@gmail.com>
2026-04-05 14:05:39 +0900

    IA integration

---
+++ Data/Scripts/Library/eawx-mod-icw/gameobjects/INSTANTACTION_MARKER_FACTION.lua
@@ -0,0 +1,27 @@
+return {
+	Fighters = {
+		["INSTANTACTION_MARKER_EMPIRE"] = {
+			DEFAULT = {Initial = 1, Reserve = 0}
+		},
+		["INSTANTACTION_MARKER_EMPIRE2"] = {
+			DEFAULT = {Initial = 1, Reserve = 0}
+		},
+		["INSTANTACTION_MARKER_EMPIRE3"] = {
+			DEFAULT = {Initial = 1, Reserve = 0}
+		},
+		["INSTANTACTION_MARKER_EMPIRE4"] = {
+			DEFAULT = {Initial = 1, Reserve = 0}
+		},
+		["INSTANTACTION_MARKER_EMPIRE5"] = {
+			DEFAULT = {Initial = 1, Reserve = 0}
+		},
+		["INSTANTACTION_MARKER_EMPIRE6"] = {
+			DEFAULT = {Initial = 1, Reserve = 0}
+		},
+		["INSTANTACTION_MARKER_PROJPROTEUS"] = {
+			DEFAULT = {Initial = 1, Reserve = 0}
+		}
+	},
+	Scripts = {"fighter-spawn"},
+	Flags = {HANGAR = true, RAPIDDEPLOY = true}
+}
